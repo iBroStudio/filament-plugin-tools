13d0119bd8fdf249502f691595bb808e36e89f0d
jahazzz <jahazzz@users.noreply.github.com>
2022-06-05 07:14:57 +0000

    Fix styling

--- tests/ReferencesFiles/Resources/TestResource/RelationManagers/BelongsToManyRelationManager.php
+++ tests/ReferencesFiles/Resources/TestResource/RelationManagers/BelongsToManyRelationManager.php
@@ -2,11 +2,9 @@
 
 namespace DummyVendor\DummyPackageValid\Filament\Resources\TestResource\RelationManagers;
 
-use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\BelongsToManyRelationManager;
 use Filament\Resources\Table;
-use Filament\Tables;
 
 class BelongsToManyRelationManager extends BelongsToManyRelationManager
 {
--- tests/ReferencesFiles/Resources/TestResource/RelationManagers/MorphManyRelationManager.php
+++ tests/ReferencesFiles/Resources/TestResource/RelationManagers/MorphManyRelationManager.php
@@ -2,11 +2,9 @@
 
 namespace DummyVendor\DummyPackageValid\Filament\Resources\TestResource\RelationManagers;
 
-use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\MorphManyRelationManager;
 use Filament\Resources\Table;
-use Filament\Tables;
 
 class MorphManyRelationManager extends MorphManyRelationManager
 {
