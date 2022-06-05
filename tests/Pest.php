2dd700cbb93bfc5d554b68bfda7d93c4b9ff6fc7
Yann FREPPEL <yann@ibro.studio>
2022-06-05 08:53:49 +0200

    Trying to fix Github actions tests (wip#3)

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,11 +10,39 @@ uses(TestCase::class)
             File::deleteDirectory(base_path('vendor'));
         }
         $this->assertDirectoryDoesNotExist(base_path('vendor'));
-        File::copyDirectory(__DIR__ . '/DummyFiles/vendor', base_path('vendor'));
-        expect(mkdir(base_path('vendor'), 0777, true))->toBe(true);
+        //File::copyDirectory(__DIR__ . '/DummyFiles/vendor', base_path('vendor'));
+        //expect(mkdir(base_path('vendor'), 0777, true))->toBe(true);
+        custom_copy(__DIR__ . '/DummyFiles/vendor', base_path('vendor'));
         $this->assertDirectoryExists(base_path('vendor'));
     })
     ->afterEach(function () {
         File::deleteDirectory(base_path('vendor'));
     })
     ->in(__DIR__);
+
+function custom_copy($src, $dst)
+{
+    // open the source directory
+    $dir = opendir($src);
+
+    // Make the destination directory if not exist
+    @mkdir($dst);
+
+    // Loop through the files in source directory
+    while( $file = readdir($dir) ) {
+
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) )
+            {
+                // Recursively calling custom copy function
+                // for sub directory
+                custom_copy($src . '/' . $file, $dst . '/' . $file);
+            }
+            else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+
+    closedir($dir);
+}
